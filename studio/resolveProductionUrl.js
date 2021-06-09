af3d353e325833d85354b12af1f37efa5e5f1fe2
Mark Plewis <markplewis1@gmail.com>
2021-06-09 08:32:11 -0400

    Experiments with preview URL env variable

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,5 +1,16 @@
+// const projectUrl = 'http://localhost:3000'
+
+// const projectUrl = process.env.SANITY_STUDIO_API_DATASET === "production"
+//   ? "https://sandiplewis-blog.vercel.app"
+//   : "https://sandiplewis-blog-dev.vercel.app";
+
+const environments = {
+  production: "https://sandiplewis-blog.vercel.app",
+  development: "https://sandiplewis-blog-dev.vercel.app",
+  local: "http://localhost:3000"
+};
+const projectUrl = environments[process.env.NEXT_PUBLIC_VERCEL_ENV] || environments[local];
 const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET;
-const projectUrl = 'http://localhost:3000'
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
