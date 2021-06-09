fa59e19f225bf084c3731e54d3cbba585b327b85
Mark Plewis <markplewis1@gmail.com>
2021-06-09 08:43:24 -0400

    More experimentation

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -9,7 +9,7 @@ const environments = {
   development: "https://sandiplewis-blog-dev.vercel.app",
   local: "http://localhost:3000"
 };
-const projectUrl = environments[process.env.NEXT_PUBLIC_VERCEL_ENV] || environments[local];
+const projectUrl = environments[process.env.SANITY_STUDIO_BUILD_ENV] || environments[local];
 const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET;
 
 export default function resolveProductionUrl(document) {
