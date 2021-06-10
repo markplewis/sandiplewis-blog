5c36cad65f7af054389e74a0262eb87745f3c1d4
Mark Plewis <markplewis1@gmail.com>
2021-06-10 09:29:19 -0400

    Cleanup

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -10,5 +10,4 @@ const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET;
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
-  // return `${projectUrl}/api/preview?secret=${previewSecret}&slug=/posts/${document.slug.current}`
 }
