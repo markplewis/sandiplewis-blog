c0eae6ebb6ca4a567a06328a59a1478260c60fe9
Mark Plewis <markplewis1@gmail.com>
2021-07-24 18:08:27 -0400

    Adds Webpack Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-module.exports = {
+// See: https://nextjs.org/docs/api-reference/next.config.js/introduction
+// Extending Next's Webpack config: https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+});
+
+module.exports = withBundleAnalyzer({
   images: {
     // These apply globally. It doesn't seem possible to specify different sizes per image.
     // https://nextjs.org/docs/basic-features/image-optimization#device-sizes
     // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     domains: ["cdn.sanity.io"]
   }
-};
+});
