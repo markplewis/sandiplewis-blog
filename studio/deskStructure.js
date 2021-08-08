374a6b7a78952c50786986b12b7bb54971179742
Mark Plewis <markplewis1@gmail.com>
2021-08-08 19:54:21 -0400

    Adds icons to Sanity Studio UI

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -3,6 +3,16 @@ import S from "@sanity/desk-tool/structure-builder";
 // https://www.sanity.io/docs/icons-for-data-types
 import EyeIcon from "part:@sanity/base/eye-icon";
 import EditIcon from "part:@sanity/base/edit-icon";
+import {
+  HiOutlineHome,
+  HiOutlineBookOpen,
+  HiOutlineUser,
+  HiOutlineDocumentText,
+  HiOutlineCog,
+  HiOutlineChat,
+  HiOutlineFolderOpen,
+  HiOutlineStar
+} from "react-icons/hi";
 
 import IframePreview from "./components/IframePreview";
 
@@ -17,10 +27,11 @@ import IframePreview from "./components/IframePreview";
 // https://www.sanity.io/blog/evolve-authoring-experiences-with-views-and-split-panes
 // https://github.com/sanity-io/gatsby-portfolio-preview-poc/blob/master/studio/README.md
 
-function splitPaneViews(title, schema, previewTitle) {
+function splitPaneViews(title, schema, previewTitle, Icon) {
   return S.listItem()
     .title(title)
     .schemaType(schema)
+    .icon(Icon)
     .child(
       S.documentTypeList(schema)
         .title(title)
@@ -44,6 +55,7 @@ const DeskStructure = () =>
       S.listItem()
         .title("Home page")
         .schemaType("homePage")
+        .icon(HiOutlineHome)
         .child(
           S.document()
             .documentId("homePage")
@@ -53,14 +65,28 @@ const DeskStructure = () =>
               S.view.component(IframePreview).icon(EyeIcon).title("Web Preview")
             ])
         ),
-      splitPaneViews("Post", "post", "Web Preview"),
-      splitPaneViews("Novel", "novel", "Web Preview"),
-      splitPaneViews("Author", "author", "Web Preview"),
-      ...S.documentTypeListItems().filter(listItem => {
-        return !["homePage", "post", "novel", "author", "settings"].includes(listItem.getId());
-      }),
+      splitPaneViews("Post", "post", "Web Preview", HiOutlineDocumentText),
+      splitPaneViews("Novel", "novel", "Web Preview", HiOutlineBookOpen),
+      splitPaneViews("Author", "author", "Web Preview", HiOutlineUser),
+      ...S.documentTypeListItems()
+        .filter(listItem => {
+          return !["homePage", "post", "novel", "author", "settings"].includes(listItem.getId());
+        })
+        .map(listItem => {
+          switch (listItem.getId()) {
+            case "category":
+              return listItem.icon(HiOutlineFolderOpen);
+            case "comment":
+              return listItem.icon(HiOutlineChat);
+            case "review":
+              return listItem.icon(HiOutlineStar);
+            default:
+              return listItem;
+          }
+        }),
       S.listItem()
         .title("Settings")
+        .icon(HiOutlineCog)
         .child(S.document().schemaType("settings").documentId("settings"))
     ]);
 
