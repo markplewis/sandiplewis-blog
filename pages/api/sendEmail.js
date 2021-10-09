a10adcded6f220cf1ad9dead6b2ccd83da1a3bfb
Mark Plewis <markplewis1@gmail.com>
2021-10-09 11:21:04 -0400

    Mailgun debugging

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -16,14 +16,18 @@ export default async function sendEmail(req, res) {
           process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
             ? "mg.sandiplewis.com"
             : "sandboxd4d6735182254bcdbd4398e2443b4d2f.mailgun.org"
-      }
+      },
+      host: "api.mailgun.net"
     })
   );
   try {
     await transporter.sendMail({
-      from: `"${name}" <messages@mg.sandiplewis.com>`,
+      from: {
+        name,
+        address: "mailgun@mg.sandiplewis.com"
+      },
       // https://help.mailgun.com/hc/en-us/articles/4401814149147-Adding-A-Reply-To-Address
-      replyTo: `"${name}" <${email}>`,
+      replyTo: email,
       // When sending email via the sandbox domain, up to 5 authorized recipients are
       // allowed on the free plan: https://help.mailgun.com/hc/en-us/articles/217531258
       to: "markplewis1@gmail.com",
